74006bf79c77c6951e8df98a7e3943c3c2a378cf
Juan Valero <valerojuan13c@gmail.com>
2021-11-11 00:10:58 -0300

    jwt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,11 +25,15 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre('save', async function(next) {
+UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
-})
+});
 
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userID: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+};
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
